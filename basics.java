de7f15489bf8f3383b0e672147fc2f5e126d97bd
aishwarya2005thakur <176339556+aishwarya2005thakur@users.noreply.github.com>
2024-09-03 23:27:43 +0530

    basic stuff

---
+++ basics.java
@@ -0,0 +1,18 @@
+public class basics {
+    public static void main(String args[]){
+        //boilerplate code \
+        int i=4;
+        
+        System.out.print("hello world\n");
+        System.out.print("My name is Aishwarya thakur.");
+        System.out .print(i);
+
+
+        
+
+
+
+
+
+        }
+}
